59e4e7c4dd72b1956e4c7e07ee4181007767b850
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-02-08 08:58:04 -0800

    Init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoMovement();
+
+        DoShooting();
+    }
+
+    private void DoShooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))//the single fram the button was pressed
+        {
+            //
+            Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        }
+
+    }
+
+    private void DoMovement()
+    {
+        if (Input.GetKey(KeyCode.A)) // if "a" key is pressed
+        {
+            //move left
+            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0); // decrease x-value
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //Move right
+            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.W))
+        {
+            //Move forward
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            //Move back
+            transform.Translate(0, 0, -moveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    /// <summary>
+    /// How fast this projectile moves.
+    /// </summary>
+    public float moveSpeed = 150;
+
+    /// <summary>
+    /// Destroys after this many seconds.
+    /// </summary>
+    public float lifeSpan = 5;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Start()
+    {
+        Destroy(this.gameObject, lifeSpan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+        Destroy(other.gameObject);
+    }
+}
