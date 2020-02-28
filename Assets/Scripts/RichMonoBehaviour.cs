be42839da3ec20375839ea6cafabd330a286cd40
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-02-28 12:58:27 -0800

    new base class that gives helpful information

---
+++ Assets/Scripts/RichMonoBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RichMonoBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// Gives information about what this object is.
+    /// </summary>
+    [TextArea]//this means a string will be 3 lines instead of a single line
+    public string developerDescription = "Please enter a description.";//
+}
