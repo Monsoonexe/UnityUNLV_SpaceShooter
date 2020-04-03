dce479de29d3e6c09d41f559127c36e69fc3682f
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-04-03 12:15:16 -0700

    new enemy controller

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This will be expanded to include movement behavior
+/// </summary>
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Probably Destroyed by a Projectile.
+    /// </summary>
+    private void OnDestroy()
+    {
+        //alert anybody who is listening that an enemy has been destroyed.
+        EventManager.onEnemyDestroyed.Invoke();
+    }
+}
