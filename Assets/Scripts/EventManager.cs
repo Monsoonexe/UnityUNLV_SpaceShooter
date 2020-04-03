82ed96fa9bd486302d69a52125c731c972865ec4
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-04-03 12:15:23 -0700

    new event manager class

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Events;
+
+/// <summary>
+/// Provides a layer of abstraction for events.
+/// </summary>
+public static class EventManager
+{
+    public static readonly UnityEvent onEnemyDestroyed = new UnityEvent();
+
+    public static readonly UnityEvent onPlayerDestroyed = new UnityEvent();
+
+    public static readonly UnityEvent onScoreUpdate = new UnityEvent();
+
+    //static means it exists as soon as the program starts
+    //NOT a MonoBehavior, so it doesn't need to be in a scene -- always exists in project
+
+    //readonly: because these are public, we don't want anyone else to do something screwy, like
+    //onEnemyDestroyed = new UnityEvent() in a different class, which will unsubscribe everyone.
+}
