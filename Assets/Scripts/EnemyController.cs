219a8baa3662c9d95fe7a9c689a7e2aae007b40c
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-04-10 14:24:15 -0700

    Added basic and shooting mechanics

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,17 +3,31 @@
 /// <summary>
 /// This will be expanded to include movement behavior
 /// </summary>
-public class EnemyController : MonoBehaviour
+public class EnemyController : RichMonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("---Attack---")]
+    [SerializeField]
+    private Vector2 shootDelay = new Vector2(1f, 3f);
+
+    private float nextShootTime = 0;
+
+    //member components
+    private ShipController myShipController;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Awake()
     {
-        
+        myShipController = gameObject.GetComponent<ShipController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        DoMovement();
+
+        DoShooting();
         
     }
 
@@ -24,5 +38,41 @@ public class EnemyController : MonoBehaviour
     {
         //alert anybody who is listening that an enemy has been destroyed.
         EventManager.onEnemyDestroyed.Invoke();
+    }
+
+    /// <summary>
+    /// This ship has collided with something.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        //kill player if collided.
+        if (other.CompareTag(PlayerController.PLAYER_TAG))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Tell ship to move.
+    /// </summary>
+    private void DoMovement()
+    {
+        Vector3 moveVector = transform.forward;
+
+        myShipController.Move(moveVector);
+    }
+
+    /// <summary>
+    /// Tell the ship to shoot.
+    /// </summary>
+    private void DoShooting()
+    {
+        if(Time.time >= nextShootTime)//cooldown
+        {
+            myShipController.Shoot();//pull trigger
+            //update delay
+            nextShootTime = Time.time + Random.Range(shootDelay.x, shootDelay.y);
+        }
     }
 }
