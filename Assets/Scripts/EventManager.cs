8194cfb97885db6bc9c1183be5c9d04d88ee64b6
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2020-04-10 14:23:38 -0700

    updated comments

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -12,7 +12,7 @@ public static class EventManager
     public static readonly UnityEvent onScoreUpdate = new UnityEvent();
 
     //static means it exists as soon as the program starts
-    //NOT a MonoBehavior, so it doesn't need to be in a scene -- always exists in project
+    //NOT a MonoBehavior, so this class doesn't need to be in a scene -- always exists in project
 
     //readonly: because these are public, we don't want anyone else to do something screwy, like
     //onEnemyDestroyed = new UnityEvent() in a different class, which will unsubscribe everyone.
